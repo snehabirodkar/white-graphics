5b6da9c751bc5b14a5d8ae2ff9d9969c27c5330d
sneha <snehabirodkar411@gmail.com>
2019-12-08 18:16:01 +0530

    team section completed

--- js/script.js
+++ js/script.js
@@ -4,4 +4,30 @@
  */
 $(window).on('load',function(){
     $("#preloader").delay(500).fadeOut('slow');
-})
+});
+$(document).ready(function(){
+    $("#team-right").owlCarousel({
+        items: 2,
+        autoplay: true,
+        margin: 20,
+        loop: true,
+        nav: true,
+        smartSpeed: 700,
+        autoplayHoverPause: true,
+        dots: false,
+        navText: ['<i class="lni-chevron-left-circle"></i>','<i class="lni-chevron-right-circle"></i>']
+    });
+});
+
+$(document).ready(function(){
+    $("#progress-elements").waypoint(function(){
+        $(".progress-bar").each(function(){
+            $(this).animate({
+                width: $(this).attr("aria-valuenow")+"%"
+            },800);
+        });
+        this.destroy();
+    },{
+        offset: 'bottom-in-view'
+    });
+});
