dcca16c790e34dfd85dfc77ff87e925b33d675b4
sneha <snehabirodkar411@gmail.com>
2019-12-09 18:15:38 +0530

    service section completed

--- js/script.js
+++ js/script.js
@@ -31,3 +31,42 @@ $(document).ready(function(){
         offset: 'bottom-in-view'
     });
 });
+
+$(document).ready(function(){
+$('#services-tabs').responsiveTabs({
+//    startCollapsed: 'accordion'
+    animation: 'slide'
+    });
+});
+
+$(document).ready(function(){
+    $("#isotope-container").isotope({});
+    $("#isotope-filters").on("click", "button", function () {
+        let filterValue = $(this).attr("data-filter");
+//        console.log(filterValue);
+        $("#isotope-container").isotope({
+            filter: filterValue
+        });
+        //active-button
+        $("#isotope-filters").find('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+$(document).ready(function(){
+    $("#portfolio-wrapper").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{
+            enabled: true
+        },
+        zoom:{
+            enabled: true,
+            duration: 300,
+            easing: 'ease-in-out',
+            opener: function (openerElement){
+                return openerElement.is('img') ? openerElement :
+                openerElement.find('img');
+            }
+        }
+    })
+})
